e77fc93ce6268084a740b831433ba1a4f4166ba7
Derek Henderson <d3r3k523@gmail.com>
2021-09-09 01:44:50 -0700

    lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -3,6 +3,13 @@
 #include <libgen.h> // for dirname()/basename()
 #include <string.h>
 
+#if 1
+    #define LOG(...) do { printf(__VA_ARGS__); } while (0)
+#else
+    #define LOG(...)
+#endif
+
+
 typedef struct node_t
 {
     char name[64]; // node's name string
@@ -10,131 +17,184 @@ typedef struct node_t
     struct node_t* child;
     struct node_t* sibling;
     struct node_t* parent;
-} NODE;
+} Node;
 
-typedef enum cmd_t
+
+void init();
+int exec_cmd(char* cmd, char* args);
+Node* search_child(Node* parent, char* name);
+int insert_child(Node* parent, Node* q);
+
+int mkdir(char* pathname);
+int rmdir(char* pathname);
+int cd(char* pathname);
+int ls(char* pathname);
+int pwd();
+int creat(char* pathname);
+int rm(char* pathname);
+int save(char* pathname);
+int reload(char* pathname);
+int menu();
+int quit();
+
+
+const char *CMD_NAMES[] = {
+    "mkdir",
+    "rmdir",
+    "cd",
+    "ls",
+    "pwd",
+    "creat",
+    "rm",
+    "save",
+    "reload",
+    "menu",
+    "quit",
+    NULL
+};
+
+int (*CMD_FUNCS[])(char*) = {
+    mkdir,
+    rmdir,
+    cd,
+    ls,
+    pwd,
+    creat,
+    rm,
+    save,
+    reload,
+    menu,
+    quit,
+};
+
+Node* root;
+Node* cwd;
+
+// Node* start;
+
+
+int main()
 {
-    MKDIR,
-    RMDIR,
-    CD,
-    LS,
-    PWD,
-    CREAT,
-    RM,
-    SAVE,
-    RELOAD,
-    MENU,
-    QUIT,
-    NONE
-} CMD;
+    init();
+    menu();
 
+    char line[128];
+    char command[16];
+    char args[64];
 
-NODE* root;
-NODE* cwd;
-NODE* start;
+    while (1)
+    {
+        printf("Enter command line : ");
+        fgets(line, 128, stdin);
+        line[strlen(line)-1] = '\0';
 
-char line[128];
-char command[16];
-char pathname[64];
+        command[0] = args[0] = '\0';
+        sscanf(line, "%s %s", command, args);
 
+        printf("command=%s pathname=%s\n", command, args);
 
-CMD findCmd(char *command)
-{
-    static const char *cmds[] = {
-        "mkdir",
-        "rmdir",
-        "cd",
-        "ls",
-        "pwd",
-        "creat",
-        "rm",
-        "save",
-        "reload",
-        "menu",
-        "quit",
-        0
-    };
-
-    for (int i = 0; cmds[i]; i++)
-        if (strcmp(command, cmds[i]) == 0)
-            return (CMD)i;
-            
-    return NONE;
+        if (command[0] != '\0')
+            exec_cmd(command, args);
+    }
 }
 
-NODE *search_child(NODE *parent, char *name)
+
+void init()
 {
-  NODE *p;
-  printf("search for %s in parent DIR\n", name);
-  p = parent->child;
-  if (p==0)
-    return 0;
-  while(p){
-    if (strcmp(p->name, name)==0)
-      return p;
-    p = p->sibling;
-  }
-  return 0;
+    root = (Node*)malloc(sizeof(Node));
+    strcpy(root->name, "/");
+    root->parent = root;
+    root->sibling = root; // ?
+    root->child = NULL;
+    root->type = 'D';
+    cwd = root;
+    printf("Root initialized OK\n");
 }
 
-int insert_child(NODE *parent, NODE *q)
+int exec_cmd(char* cmd, char* args)
 {
-  NODE *p;
-  printf("insert NODE %s into END of parent child list\n", q->name);
-  p = parent->child;
-  if (p==0)
-    parent->child = q;
-  else{
-    while(p->sibling)
-      p = p->sibling;
-    p->sibling = q;
-  }
-  q->parent = parent;
-  q->child = 0;
-  q->sibling = 0;
+    for (int i = 0; CMD_NAMES[i]; i++)
+        if (strcmp(cmd, CMD_NAMES[i]) == 0)
+            return CMD_FUNCS[i](args);
+    return -1;
 }
+
+Node* search_child(Node* parent, char* name)
+{
+    Node* p;
+    LOG("search for %s in parent DIR\n", name);
+    p = parent->child;
+    while (p)
+    {
+        if (strcmp(p->name, name) == 0)
+            return p;
+        p = p->sibling;
+    }
+    return NULL;
+}
+
+int insert_child(Node* parent, Node* q)
+{
+    Node* p;
+    printf("insert NODE %s into END of parent child list\n", q->name);
+    p = parent->child;
+    if (p == NULL)
+        parent->child = q;
+    else
+    {
+        while (p = p->sibling);
+        p->sibling = q;
+    }
+    q->parent = parent;
+    q->child = NULL;
+    q->sibling = NULL;
+}
+
 
 /***************************************************************
  This mkdir(char *name) makes a DIR in the current directory
  You MUST improve it to mkdir(char *pathname) for ANY pathname
 ****************************************************************/
 
-int mkdir(char *name)
+int mkdir(char* name)
 {
-  NODE *p, *q;
-  printf("mkdir: name=%s\n", name);
+    Node* p;
+    Node* q;
+    printf("mkdir: name=%s\n", name);
 
-  if (!strcmp(name, "/") || !strcmp(name, ".") || !strcmp(name, "..")){
-    printf("can't mkdir with %s\n", name);
-    return -1;
-  }
-  if (name[0]=='/')
-    start = root;
-  else
-    start = cwd;
+    if (!strcmp(name, "/") || !strcmp(name, ".") || !strcmp(name, ".."))
+    {
+        printf("can't mkdir with %s\n", name);
+        return -1;
+    }
+    if (name[0] == '/')
+        start = root;
+    else
+        start = cwd;
 
-  printf("check whether %s already exists\n", name);
-  p = search_child(start, name);
-  if (p){
-    printf("name %s already exists, mkdir FAILED\n", name);
-    return -1;
-  }
-  printf("--------------------------------------\n");
-  printf("ready to mkdir %s\n", name);
-  q = (NODE *)malloc(sizeof(NODE));
-  q->type = 'D';
-  strcpy(q->name, name);
-  insert_child(start, q);
-  printf("mkdir %s OK\n", name);
-  printf("--------------------------------------\n");
-    
-  return 0;
+    printf("check whether %s already exists\n", name);
+    p = search_child(start, name);
+    if (p)
+    {
+        printf("name %s already exists, mkdir FAILED\n", name);
+        return -1;
+    }
+    printf("--------------------------------------\n");
+    printf("ready to mkdir %s\n", name);
+    q = (Node*)malloc(sizeof(Node));
+    q->type = 'D';
+    strcpy(q->name, name);
+    insert_child(start, q);
+    printf("mkdir %s OK\n", name);
+    printf("--------------------------------------\n");
+
+    return 0;
 }
 
 // This ls() list CWD. You MUST improve it to ls(char *pathname)
-void ls(char* pathname)
+int ls(char* pathname)
 {
-    NODE* p = cwd->child;
+    Node* p = cwd->child;
     printf("%s contents = ", pathname);
     while (p)
     {
@@ -144,68 +204,18 @@ void ls(char* pathname)
     printf("\n");
 }
 
-void quit()
+int menu()
+{
+    printf("commands = [mkdir|rmdir|cd|ls|pwd|creat|rm|save|reload|menu|quit]\n");
+}
+
+int quit()
 {
     printf("Program exit\n");
 
-
-
+    save('filetree.txt');
 
     exit(0);
     // improve quit() to SAVE the current tree as a Linux file
     // for reload the file to reconstruct the original tree
 }
-
-void menu()
-{
-    printf("commands = [mkdir|rmdir|cd|ls|pwd|creat|rm|save|reload|menu|quit]\n");
-}
-
-void initialize()
-{
-    root = (NODE*)malloc(sizeof(NODE));
-    strcpy(root->name, "/");
-    root->parent = root;
-    root->sibling = 0;
-    root->child = 0;
-    root->type = 'D';
-    cwd = root;
-    printf("Root initialized OK\n");
-}
-
-int main()
-{
-    initialize();
-    menu();
-
-    while (1)
-    {
-        printf("Enter command line : ");
-        fgets(line, 128, stdin);
-        line[strlen(line)-1] = 0;
-
-        command[0] = pathname[0] = 0;
-        sscanf(line, "%s %s", command, pathname);
-        printf("command=%s pathname=%s\n", command, pathname);
-
-        if (command[0] == 0) 
-            continue;
-
-        CMD cmd = findCmd(command);
-        switch (cmd)
-        {
-            case MKDIR  : mkdir(pathname);  break;
-            case RMDIR  : rmdir(pathname);  break;
-            case CD     : cd(pathname);     break;
-            case LS     : ls(pathname);     break;
-            case PWD    : pwd();            break;
-            case CREAT  : creat(pathname);  break;
-            case RM     : rm(pathname);     break;
-            case SAVE   : save(pathname);   break;
-            case RELOAD : reload(pathname); break;
-            case MENU   : menu();           break;
-            case QUIT   : quit();           break;
-        }
-    }
-}
-

325ddcd06f77b7d7eb2d344b22427d23cb9d67aa
D3r3k23 <d3r3k523@gmail.com>
2021-12-07 04:19:15 -0800

    cmd_mv

--- project/src/write_cp.c
+++ project/src/write_cp.c
@@ -19,5 +19,18 @@ void cmd_cp(char* src, char* dest)
 
 void cmd_mv(char* src, char* dest)
 {
+    u32 src_ino  = getino(src);
+    u32 dest_ino = getino(dest);
 
+    MINODE* src_mip  = iget(src_ino);
+    MINODE* dest_mip = iget(dest_ino);
+
+    if (src_mip->dev == dest_mip->dev)
+    {
+        
+    }
+    else
+    {
+        // Level 3
+    }
 }

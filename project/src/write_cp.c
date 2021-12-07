6a52be951dc1aa93098f798bd0bbe9dfa1c47dab
D3r3k23 <d3r3k523@gmail.com>
2021-12-07 04:15:36 -0800

    src/dest

--- project/src/write_cp.c
+++ project/src/write_cp.c
@@ -12,12 +12,12 @@ int my_write(int fd, char* in_buf, size_t count)
 
 }
 
-void cmd_cp(char* dest, char* src)
+void cmd_cp(char* src, char* dest)
 {
     
 }
 
-void cmd_mv(char* dest, char* src)
+void cmd_mv(char* src, char* dest)
 {
 
 }
--- project/src/write_cp.h
+++ project/src/write_cp.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 int my_write(int fd, char* in_buf, size_t count);
-void cmd_cp(char* dest, char* src);
-void cmd_mv(char* dest, char* src);
+void cmd_cp(char* src, char* dest);
+void cmd_mv(char* src, char* dest);
 
 #endif // WRITE_CP_H
